bd3936016d86d14f5b7affc21b7b31f0219a2ea9
mmsfloor <mmerk9@gmail.com>
2018-11-30 23:54:17 +0200

    s

--- JS-3/js/main2.js
+++ JS-3/js/main2.js
@@ -1,3 +1,41 @@
-(function(){
+(function(){    
+
+    area.addEventListener('click', function (e) {
+
+
+        var target = e.target;
+        
+        if (target != area) return;
+
+        var el = document.createElement('div');
+        var color = newColor.value;
+        if(newColor.value == '#ffffff') {
+            el.style.border = '1px solid #000'
+        }
+        el.classList.add('figure');
+        el.style.top = e.offsetY + 'px';
+        el.style.left = e.offsetX + 'px';
+        el.style.backgroundColor = color;
+        var figureInput = document.querySelectorAll('[name="figure"]');
+        for (var i = 0; i < figureInput.length; i++) {
+            if (figureInput[i].checked) {
+                el.classList.add(figureInput[i].value);
+                var elNum = i;
+            }
+        }
+
+        if(figureInput[elNum].value == 'ellipse' || figureInput[elNum].value == 'rectangle') {
+                el.style.width = newWidth.value + 'px';
+                el.style.height = newHeight.value + 'px';
+        }
+
+        if (newWidth.value>=area.offsetWidth || newHeight.value>=area.offsetHeight) {
+            alert('Width and height cannot exceed canvas size: '+ 
+                area.offsetWidth + 'px * '+ area.offsetHeight + 'px');
+            return;
+        }
+        
+        target.appendChild(el);    
+    })
 
 }());
